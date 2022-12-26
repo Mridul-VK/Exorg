9aa8db0118d2f14c50d9ac4a10b2bb91a511e287
Mridul-VK <72720710+Mridul-VK@users.noreply.github.com>
2022-12-26 05:06:45 +0000

    some minor bug fixes

--- frontend/src/components/OrgTypeCard.tsx
+++ frontend/src/components/OrgTypeCard.tsx
@@ -11,14 +11,15 @@ interface propInterface {
 }
 
 const OrgTypeCard = (props:propInterface) => {
+    const {name, image, color, btn} = props.cardtype
   return (
-    <div className={`h-full w-60 border rounded-3xl border-${props.cardtype.color} overflow-hidden my-auto`}>
+    <div className={`h-full w-60 border rounded-3xl border-${color} overflow-hidden my-auto`}>
         <div className="h-3/4 flex flex-col items-center justify-between py-6">
-            <img src={props.cardtype.image} className="h-2/3" />
-            <span className={`text-${props.cardtype.color} font-medium text-lg`}>{props.cardtype.name}</span>
+            <img src={image} className="h-2/3" />
+            <span className={`text-${color} font-medium text-lg`}>{name}</span>
         </div>
-        <div className='h-1/4'>
-            <button className={`bg-${props.cardtype.color} text-gray-900 w-full h-full text-xl`}>{props.cardtype.btn}</button>
+        <div className={`h-1/4 bg-${color}`}>
+            <button type="button" className={`text-gray-900 w-full h-full text-xl`}>{btn}</button>
         </div>
     </div>
   )
